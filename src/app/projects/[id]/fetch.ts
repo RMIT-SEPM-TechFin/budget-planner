5aac4b70ad81afd2a5f60445510abeb9d16a003b
Cullen179 <itslamemail@gmail.com>
2024-04-17 01:11:15 +0700

    Fetch categories, items

--- src/app/projects/[id]/fetch.ts
+++ src/app/projects/[id]/fetch.ts
@@ -1,22 +1,32 @@
-import {
-  collection,
-  doc,
-  documentId,
-  FieldValue,
-  getDocs,
-  orderBy,
-  query,
-  where,
-} from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 
 import db from '@/firebase/db';
-import { Item } from '@/types';
+import { Category, Item } from '@/types';
 
 export default async function fetchItemData(projectId: string) {
   // Reference to the project document
   const projectRef = doc(db, 'projects', projectId);
-  //   // Reference to the items subcollection within the project document
+
+  const project = (await getDoc(projectRef)).data();
+  if (!project) {
+    throw new Error('Project not found');
+  }
+  const name = project.name;
+
+  // Reference to the items subcollection within the project document
   const itemsRef = collection(projectRef, 'items');
+
+  const categoryRef = collection(projectRef, 'categories');
+
+  const categories = await getDocs(categoryRef).then((snapshot) => {
+    return snapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        ...data,
+        id: doc.id,
+      } as Category;
+    });
+  });
 
   const items = await getDocs(itemsRef).then((snapshot) => {
     return snapshot.docs.map((doc) => {
@@ -24,8 +34,11 @@ export default async function fetchItemData(projectId: string) {
       return {
         ...data,
         id: doc.id,
+        category:
+          categories.find((category) => category.id === data.category)?.id ||
+          'Unknown',
       } as Item;
     });
   });
-  return items;
+  return { name, items, categories };
 }
