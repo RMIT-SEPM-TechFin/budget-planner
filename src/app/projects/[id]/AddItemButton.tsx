ebe165261d485d2a15225ee362fb9012c13eb7de
Cullen179 <itslamemail@gmail.com>
2024-04-17 01:10:57 +0700

    Update AddItemButton.tsx

--- src/app/projects/[id]/AddItemButton.tsx
+++ src/app/projects/[id]/AddItemButton.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FC, useCallback, useState, useTransition } from 'react';
+import { FC, useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -7,74 +7,19 @@ import {
   DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-} from '@/components/ui/select';
-import { Textarea } from '@/components/ui/textarea';
-import useNotification from '@/hooks/useNotification';
+import { Category } from '@/types';
 
-import { saveNewItem } from './actions';
-import SelectCategory from './SelectCategory';
+import ItemForm from './ItemForm';
 
-const AddItemButton: FC<{ className?: string; projectId: string }> = ({
+const AddItemButton: FC<{ className?: string; categories: Category[] }> = ({
   className,
-  projectId,
+  categories,
 }) => {
-  const { showNotification } = useNotification();
-  const [_, startTransition] = useTransition();
-
   const [open, setOpen] = useState(false);
-
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [price, setPrice] = useState<number>(0);
-  const [quantity, setQuantity] = useState<number>(0);
-  const [category, setCategory] = useState('');
-
-  const handleSaveNewItem = useCallback(() => {
-    startTransition(() => {
-      try {
-        saveNewItem(projectId, {
-          category: category,
-          description: description,
-          name: name,
-          price: price,
-          quantity: quantity,
-        });
-        setOpen(false);
-        showNotification({
-          title: 'New Item Created',
-          variant: 'success',
-        });
-      } catch {
-        showNotification({
-          title: 'Failed to create new Item',
-          variant: 'failure',
-        });
-      }
-    });
-  }, [
-    projectId,
-    category,
-    name,
-    description,
-    price,
-    quantity,
-    showNotification,
-    startTransition,
-  ]);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -89,82 +34,7 @@ const AddItemButton: FC<{ className?: string; projectId: string }> = ({
           <DialogDescription>Add the item details below</DialogDescription>
           <DialogClose />
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="category">Select</Label>
-
-            {/* TODO: input turns white when selecting existing option */}
-
-            <SelectCategory name="category" className="col-span-3" />
-          </div>
-
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="item-name">Name</Label>
-            {/* TODO: input turns white when selecting existing option */}
-            <Input
-              id="item-name"
-              className="col-span-3"
-              placeholder="New Item"
-              name="name"
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-            />
-          </div>
-
-          <div className="flex-col flex items-start gap-4">
-            <Label htmlFor="item-description" className="col-span-4 ">
-              Description
-            </Label>
-
-            <Textarea
-              id="item-description"
-              name="description"
-              placeholder="Type the item description"
-              value={description}
-              onChange={(e) => {
-                setDescription(e.target.value);
-              }}
-            />
-          </div>
-
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="price">Price</Label>
-            <Input
-              id="price"
-              className="col-span-3"
-              placeholder="Price"
-              type="number"
-              name="price"
-              value={price}
-              onChange={(e) => {
-                setPrice(Number(e.target.value));
-              }}
-            />
-          </div>
-
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="Quantity">Quantity</Label>
-            <Input
-              id="Quantity"
-              className="col-span-3"
-              placeholder="Quantity"
-              type="number"
-              name="quantity"
-              value={quantity}
-              onChange={(e) => {
-                setQuantity(Number(e.target.value));
-              }}
-            />
-          </div>
-        </div>
-
-        <DialogFooter>
-          <Button type="submit" onClick={handleSaveNewItem}>
-            Add
-          </Button>
-        </DialogFooter>
+        <ItemForm categories={categories} />
       </DialogContent>
     </Dialog>
   );
