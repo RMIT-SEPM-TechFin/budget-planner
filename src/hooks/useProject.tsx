e969a3927b8da285664881d86e213f9bdc19c4df
Cullen179 <itslamemail@gmail.com>
2024-04-17 01:14:26 +0700

    useProject to get project id

---
+++ src/hooks/useProject.tsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+
+import { ProjectContext } from '@/context/ProjectContext';
+
+const useProject = () => {
+  return useContext(ProjectContext);
+};
+
+export default useProject;
