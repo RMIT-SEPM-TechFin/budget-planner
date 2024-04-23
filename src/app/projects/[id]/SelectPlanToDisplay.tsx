17472226724f65f806563aa7e02c3611d85feb53
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-04-24 01:17:42 +0700

    Allow add, edit plan

--- src/app/projects/[id]/SelectPlanToDisplay.tsx
+++ src/app/projects/[id]/SelectPlanToDisplay.tsx
@@ -1,7 +1,18 @@
 'use client';
 
-import { FC } from 'react';
+import { MoreVertical, Plus } from 'lucide-react';
+import { FC, useState } from 'react';
 
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import {
   Select,
   SelectContent,
@@ -15,6 +26,7 @@ import {
 import type { Plan } from '@/types';
 
 import { usePlanIdQueryParam } from './hooks';
+import PlanForm from './PlanForm';
 
 interface SelectPlanProps {
   plans: Plan[];
@@ -22,6 +34,8 @@ interface SelectPlanProps {
 
 const SelectPlanToDisplay: FC<SelectPlanProps> = ({ plans }) => {
   const { planId, setPlanId } = usePlanIdQueryParam();
+  const [openAdd, setOpenAdd] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
 
   return (
     <Select
@@ -37,12 +51,62 @@ const SelectPlanToDisplay: FC<SelectPlanProps> = ({ plans }) => {
         <SelectItem value="all">All Items</SelectItem>
         <SelectSeparator />
         <SelectGroup>
-          <SelectLabel>Plans</SelectLabel>
-          {plans.map((plan) => (
-            <SelectItem value={plan.id} key={plan.id}>
-              {plan.name}
-            </SelectItem>
-          ))}
+          <div className="relative">
+            <SelectLabel>Plans</SelectLabel>
+            <Dialog open={openAdd} onOpenChange={setOpenAdd}>
+              <DialogTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="absolute h-8 w-8 right-0 top-0 z-100"
+                >
+                  <span className="sr-only">Open menu</span>
+                  <Plus className="h-4 w-4 absolute" />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[1000px]">
+                <DialogHeader>
+                  <DialogTitle>Create New Plan</DialogTitle>
+                  <DialogDescription>
+                    Choose the items you want to display
+                  </DialogDescription>
+                  <DialogClose />
+                </DialogHeader>
+
+                {/* Plan Form for creating new Plan */}
+                <PlanForm onCloseForm={() => setOpenAdd(false)}/>
+              </DialogContent>
+            </Dialog>
+          </div>
+          <div className="relative">
+            {plans.map((plan) => (
+              <div className="relative z-0" key={plan.id}>
+                <SelectItem value={plan.id}>{plan.name}</SelectItem>
+                <Dialog open={openEdit} onOpenChange={setOpenEdit}>
+                  <DialogTrigger asChild>
+                    <Button
+                      variant="ghost"
+                      className="absolute h-8 w-8 right-0 top-0 z-100"
+                    >
+                      <span className="sr-only">Open menu</span>
+                      <MoreVertical className="h-4 w-4 absolute" />
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[1000px]">
+                    <DialogHeader>
+                      <DialogTitle>Edit Plan {plan.name}</DialogTitle>
+                      <DialogDescription>
+                        Edit the items you want to display
+                      </DialogDescription>
+                      <DialogClose />
+                    </DialogHeader>
+
+                    {/* Plan Form for editting Plan */}
+                    <PlanForm editPlanData={plan} onCloseForm={() => setOpenEdit(false)}/>
+                  </DialogContent>
+                </Dialog>
+              </div>
+            ))}
+          </div>
           {plans.length === 0 && 'No plans available'}
         </SelectGroup>
       </SelectContent>
