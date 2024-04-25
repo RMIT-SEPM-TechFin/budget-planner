81654c660f001850b6e2b98973d91421bf3cd12c
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-04-25 20:30:03 +0700

    chore: fix edit plan modal only show one plan

--- src/app/projects/[id]/SelectPlanToDisplay.tsx
+++ src/app/projects/[id]/SelectPlanToDisplay.tsx
@@ -78,37 +78,39 @@ const SelectPlanToDisplay: FC<SelectPlanProps> = ({ plans }) => {
             </Dialog>
           </div>
           <div className="relative">
-            {plans.map((plan) => (
-              <div className="relative z-0" key={plan.id}>
-                <SelectItem value={plan.id}>{plan.name}</SelectItem>
-                <Dialog open={openEdit} onOpenChange={setOpenEdit}>
-                  <DialogTrigger asChild>
-                    <Button
-                      variant="ghost"
-                      className="absolute h-8 w-8 right-0 top-0 z-100"
-                    >
-                      <span className="sr-only">Open menu</span>
-                      <MoreVertical className="h-4 w-4 absolute" />
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[1000px] overflow-y-scroll max-h-screen">
-                    <DialogHeader>
-                      <DialogTitle>Edit Plan {plan.name}</DialogTitle>
-                      <DialogDescription>
-                        Edit the items you want to display
-                      </DialogDescription>
-                      <DialogClose />
-                    </DialogHeader>
+            {plans.map((plan) => {
+              return (
+                <div className="relative z-0" key={plan.id}>
+                  <SelectItem value={plan.id}>{plan.name}</SelectItem>
+                  <Dialog key={plan.id}>
+                    <DialogTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        className="absolute h-8 w-8 right-0 top-0 z-100"
+                      >
+                        <span className="sr-only">Open menu</span>
+                        <MoreVertical className="h-4 w-4 absolute" />
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[1000px] overflow-y-scroll max-h-screen">
+                      <DialogHeader>
+                        <DialogTitle>Edit Plan {plan.name}</DialogTitle>
+                        <DialogDescription>
+                          Edit the items you want to display
+                        </DialogDescription>
+                        <DialogClose />
+                      </DialogHeader>
 
-                    {/* Plan Form for editting Plan */}
-                    <PlanForm
-                      editPlanData={plan}
-                      onCloseForm={() => setOpenEdit(false)}
-                    />
-                  </DialogContent>
-                </Dialog>
-              </div>
-            ))}
+                      {/* Plan Form for editing Plan */}
+                      <PlanForm
+                        editPlanData={plan}
+                        onCloseForm={() => setOpenEdit(false)}
+                      />
+                    </DialogContent>
+                  </Dialog>
+                </div>
+              );
+            })}
           </div>
           {plans.length === 0 && 'No plans available'}
         </SelectGroup>
