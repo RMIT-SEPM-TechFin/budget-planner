327301c4d037ff40e3327addb02a0932ffdcc39e
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-04-26 15:50:30 +0700

    chore: Layout for comparison

---
+++ src/app/comparison/[id]/layout.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from 'react';
+
+import { Button } from '@/components/ui/button';
+
+import { ProjectContextProvider } from '../../projects/[id]/context';
+import {
+  fetchProjectItemsAndCategories,
+  fetchProjectName,
+  fetchProjectPlans,
+} from '../../projects/[id]/fetch';
+
+export default async function Layout({
+  params,
+  children,
+}: {
+  params: { id: string };
+  children: ReactNode;
+}) {
+  const { id } = params;
+
+  const [name, plans, { items, categories }] = await Promise.all([
+    fetchProjectName(id),
+    fetchProjectPlans(id),
+    fetchProjectItemsAndCategories(id),
+  ]);
+
+  return (
+    <ProjectContextProvider
+      projectId={id}
+      categories={categories}
+      items={items}
+      plans={plans}
+    >
+      <div className="space-y-6">
+        <h1>{name}</h1>
+        <div className="relative">{children}</div>
+      </div>
+    </ProjectContextProvider>
+  );
+}
