5b032613bd7289ae2907d67d9e9480eba809e940
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-04-29 17:25:30 +0700

    Update ItemTable.tsx

--- src/app/comparison/[id]/ItemTable.tsx
+++ src/app/comparison/[id]/ItemTable.tsx
@@ -8,15 +8,8 @@ import {
   TableActionCell,
   TableSortableHeader,
 } from '@/components/table';
-import {
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
 import useNotification from '@/hooks/useNotification';
-import type { Item } from '@/types';
+import type { Category, Item } from '@/types';
 
 import { deleteItem } from '../../projects/[id]/actions';
 import { useProject } from '../../projects/[id]/context';
@@ -49,46 +42,7 @@ const staticColumns = [
   }),
 ] as ColumnDef<Item>[];
 
-const ItemTable: FC<{ planId: string | undefined }> = ({ planId }) => {
-  const [_, startTransition] = useTransition();
-  const { showNotification } = useNotification();
-  const { projectId, categories, items, plans } = useProject();
-
-  const filteredItems = useMemo(() => {
-    const filteredItemIds =
-      typeof planId === 'string'
-        ? plans.find((plan) => plan.id === planId)?.items
-        : undefined;
-
-    if (filteredItemIds === undefined) return items;
-
-    console.log('Filtered Item IDs:', filteredItemIds);
-
-    return items.filter((item) => filteredItemIds.includes(item.id));
-  }, [planId, plans, items]);
-
-  const handleDelete = useCallback(
-    (id: string) => {
-      startTransition(() => {
-        try {
-          const planIdsContainItem = plans
-            .filter((plan) => plan.items.includes(id))
-            .map((plan) => plan.id);
-          deleteItem(projectId, id, planIdsContainItem);
-          showNotification({
-            title: 'Item deleted',
-            variant: 'success',
-          });
-        } catch {
-          showNotification({
-            title: 'Failed to delete item',
-            variant: 'failure',
-          });
-        }
-      });
-    },
-    [projectId, showNotification, plans],
-  );
+const ItemTable: FC<{ planId: string | undefined, filteredItems: Item[], categories: Category[] }> = ({ planId, filteredItems, categories }) => {
 
   const columns = useMemo(
     () =>
