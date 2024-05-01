8384fac79441c3f30eb4afd27dfdfc08c2acf6e8
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-05-01 18:21:35 +0700

    Update table.tsx

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -60,7 +60,7 @@ const TableRow = React.forwardRef<
     className={cn(
       'border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
       className,
-    )}
+    )}  
     {...props}
   />
 ));
