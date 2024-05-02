14b54e237eac263425d2310d9ececf6aba8a1dca
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-05-02 12:21:04 +0700

    Update Routing (Project)

--- src/app/projects/[id]/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { StringParam, useQueryParam } from 'use-query-params';
-
-export const usePlanIdQueryParam = () => {
-  const [planId, setPlanId] = useQueryParam('planId', StringParam);
-
-  return { planId, setPlanId };
-};
