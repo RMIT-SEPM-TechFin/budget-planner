7f26d34c5bad466d85a38a7f7e7f72de1a71555b
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-05-07 01:41:13 +0700

    Update layout.tsx

--- src/app/projects/[id]/comparison/layout.tsx
+++ src/app/projects/[id]/comparison/layout.tsx
@@ -1,15 +1,4 @@
-import Link from 'next/link';
 import { ReactNode } from 'react';
-
-import Breadcrumbs from '@/components/Breadcrumbs';
-import { Button } from '@/components/ui/button';
-
-import { ProjectContextProvider } from '../(project)/context';
-import {
-  fetchProjectInfo,
-  fetchProjectItemsAndCategories,
-  fetchProjectPlans,
-} from '../(project)/fetch';
 
 export default async function Layout({
   params,
@@ -20,40 +9,9 @@ export default async function Layout({
 }) {
   const { id } = params;
 
-  const [{ name, members }, plans, { items, categories }] = await Promise.all([
-    fetchProjectInfo(id),
-    fetchProjectPlans(id),
-    fetchProjectItemsAndCategories(id),
-  ]);
-
   return (
-    <ProjectContextProvider
-      projectId={id}
-      categories={categories}
-      items={items}
-      plans={plans}
-    >
-      <div className="space-y-10">
-        <Breadcrumbs
-          items={[
-            { label: 'Dashboard', href: '/dashboard' },
-            { label: name, href: `/projects/${id}` },
-            { label: 'Comparison' },
-          ]}
-        />
-        <div className="flex justify-between">
-          <h1>{name}</h1>
-          <Link href={`/projects/${id}`}>
-            <Button
-              className="bg-white border-[1px] border-[#e4e4e7]"
-              variant="secondary"
-            >
-              Back to Project Page
-            </Button>
-          </Link>
-        </div>
+      <div className="space-y-4">
         <div className="relative">{children}</div>
       </div>
-    </ProjectContextProvider>
   );
 }
