d823d2026c48784eaa605b9ee8412c70c40f0cf5
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-05-07 01:42:32 +0700

    Create context.tsx

---
+++ src/app/projects/[id]/context.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { createContext, FC, PropsWithChildren, useContext } from 'react';
+
+import type { Category, Item, Plan } from '@/types';
+
+interface ProjectContextType {
+  projectId: string;
+  categories: Category[];
+  items: Item[];
+  plans: Plan[];
+  name: string;
+  members: string[];
+}
+
+export const ProjectContext = createContext<ProjectContextType>({
+  projectId: '',
+  categories: [],
+  items: [],
+  plans: [],
+  name: '',
+  members: [], 
+} as ProjectContextType);
+
+export const ProjectContextProvider: FC<
+  PropsWithChildren<ProjectContextType>
+> = ({ children, projectId, categories, items, plans, name, members }) => {
+  return (
+    <ProjectContext.Provider value={{ projectId, categories, items, plans, name, members }}>
+      {children}
+    </ProjectContext.Provider>
+  );
+};
+
+export const useProject = () => {
+  return useContext(ProjectContext);
+};
