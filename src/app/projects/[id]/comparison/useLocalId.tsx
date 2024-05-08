1aa0823dc538967542e7fd96bd8b21c4d4f53b58
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-05-08 14:43:27 +0700

    Delete useLocalId.tsx

--- src/app/projects/[id]/comparison/useLocalId.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-// This is a new hook for local state management
-export const usePlanIdLocal = () => {
-  const [planId, setPlanId] = useState<string | undefined>(undefined);
-
-  return { planId, setPlanId };
-};
